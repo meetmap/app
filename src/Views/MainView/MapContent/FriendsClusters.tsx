fd18dd518e8d27850296b3a5d26fdaf35916e8f5
davidka2003 <d4v1ds0n.p@gmail.com>
2023-07-25 03:01:45 +0300

    update plist, disabled map rotation

--- src/Views/MainView/MapContent/FriendsClusters.tsx
+++ src/Views/MainView/MapContent/FriendsClusters.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../store/hooks';
-import { Marker } from 'react-native-maps';
-import { getUpdatedFriendsLocationThunk } from '../../../store/slices/locationSlice';
+import React, {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from '../../../store/hooks';
+import {Marker} from 'react-native-maps';
+import {getUpdatedFriendsLocationThunk} from '../../../store/slices/locationSlice';
 import FriendPin from '../../../shared/Pins/FriendPin';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+import Animated, {FadeIn, FadeOut} from 'react-native-reanimated';
 import FriendMarker from './FriendMarker';
 
 // const AnimatedMarker = Animated.createAnimatedComponent(Marker);
@@ -18,18 +18,18 @@ const FriendsClusters = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const { friendsCoordinates } = useAppSelector(state => state.locationSlice);
+  const {friendsCoordinates} = useAppSelector(state => state.locationSlice);
   const mapFiler = useAppSelector(state => state.mapSlice.mapFilters);
   if (mapFiler === 'Friends' || mapFiler === 'All') {
     return (
-      friendsCoordinates.map(friend => {
-        if (friend.location) {
-          return (
-            <FriendMarker key={friend.id} friend={friend} />
-          )
-        }
-        return null
-      })
+      <>
+        {friendsCoordinates.map(friend => {
+          if (friend.location) {
+            return <FriendMarker key={friend.id} friend={friend} />;
+          }
+          return null;
+        })}
+      </>
     );
   }
   return null;
--- src/Views/MainView/MapContent/index.tsx
+++ src/Views/MainView/MapContent/index.tsx
@@ -1,102 +1,122 @@
-import { LegacyRef, RefObject, createContext, useCallback, useEffect, useRef, useState } from "react";
-import MapView, { Details, Marker, Region } from "react-native-maps"
-import { useAppDispatch, useAppSelector } from "../../../store/hooks";
-import { setAddressState } from "../../../store/slices/mapSlice";
-import UserMarker from "./UserMarker";
-import { useMap } from "../../../hooks/MapProvider";
-import { debounce } from "lodash";
-import { Dimensions } from "react-native";
-import { getEventsByLocationThunk } from "../../../store/slices/eventsSlice";
-import useSupercluster, { ClusterPoint } from "../../../hooks/useSuperCluster";
-import EventsClusters from "./EventsClusters";
-import { IEvent } from "../../../types/event";
-import { store } from "../../../store/store";
-import FriendsClusters from "./FriendsClusters";
-
+import {
+  LegacyRef,
+  RefObject,
+  createContext,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import MapView, {Details, Marker, Region} from 'react-native-maps';
+import {useAppDispatch, useAppSelector} from '../../../store/hooks';
+import {setAddressState} from '../../../store/slices/mapSlice';
+import UserMarker from './UserMarker';
+import {useMap} from '../../../hooks/MapProvider';
+import {debounce} from 'lodash';
+import {Dimensions} from 'react-native';
+import {getEventsByLocationThunk} from '../../../store/slices/eventsSlice';
+import useSupercluster, {ClusterPoint} from '../../../hooks/useSuperCluster';
+import EventsClusters from './EventsClusters';
+import {IEvent} from '../../../types/event';
+import {store} from '../../../store/store';
+import FriendsClusters from './FriendsClusters';
 
 const MapContent = () => {
-    const { mapViewRef } = useMap();
-    const { userCoordinates } = useAppSelector(state => state.locationSlice)
+  const {mapViewRef} = useMap();
+  const {userCoordinates} = useAppSelector(state => state.locationSlice);
 
-    const dispatch = useAppDispatch()
-    const [clusters, setClusters] = useState<ClusterPoint[]>([])
-    const mapFiler = useAppSelector(state => state.mapSlice.mapFilters)
+  const dispatch = useAppDispatch();
+  const [clusters, setClusters] = useState<ClusterPoint[]>([]);
+  const mapFiler = useAppSelector(state => state.mapSlice.mapFilters);
 
+  const handleRegionChangeComplete = async (
+    region: Region,
+    details: Details,
+  ) => {
+    const data = await mapViewRef.current?.addressForCoordinate({
+      latitude: region.latitude,
+      longitude: region.longitude,
+    });
+    dispatch(setAddressState(data));
 
-    const handleRegionChangeComplete = async (region: Region, details: Details) => {
-        const data = await mapViewRef.current?.addressForCoordinate({ latitude: region.latitude, longitude: region.longitude })
-        dispatch(setAddressState(data))
+    if (mapFiler === 'Friends') {
+      return;
+    }
 
-        if (mapFiler === "Friends") {
-            return;
-        }
+    const scale =
+      (156_543.03392 * Math.cos((region.latitude * Math.PI) / 180)) /
+      2 ** zoomLevel;
+    const visibleRadius = ((scale * windowWidth) / 2 / 1000) * 10;
+    await dispatch(
+      getEventsByLocationThunk({
+        lat: region.latitude,
+        lng: region.longitude,
+        radius: visibleRadius,
+      }),
+    );
+  };
 
-        const scale = 156_543.03392 * Math.cos(region.latitude * Math.PI / 180) / 2 ** zoomLevel
-        const visibleRadius = scale * windowWidth / 2 / 1000 * 10;
-        await dispatch(getEventsByLocationThunk(
-            {
-                lat: region.latitude,
-                lng: region.longitude,
-                radius: visibleRadius
-            }
-        ))
-    };
+  const windowWidth = Dimensions.get('window').width;
+  const [zoomLevel, setZoomLevel] = useState(20);
 
+  const handleRegionChange = debounce(
+    async (region: Region) => {
+      if (mapFiler === 'Friends') {
+        return;
+      }
+      const zoomLevelI =
+        Math.log2(360 * (windowWidth / 256 / region.longitudeDelta)) + 1;
+      setZoomLevel(zoomLevelI);
+      const boundsData = await mapViewRef.current?.getMapBoundaries();
+      if (!boundsData) {
+        return;
+      }
+      const clustersData = useSupercluster({
+        points: store.getState().eventsSlice.eventsGeo,
+        bounds: [
+          boundsData?.southWest.longitude - 0.02,
+          boundsData?.southWest.latitude - 0.02,
+          boundsData?.northEast.longitude + 0.02,
+          boundsData?.northEast.latitude + 0.02,
+        ],
+        zoom: zoomLevel,
+        options: {
+          radius: 75,
+          maxZoom: 40,
+          minZoom: 2,
+          reduce: (accumulated, props) => {
+            accumulated.ids = [...accumulated.ids, ...props.ids];
+          },
+        },
+      });
 
-    const windowWidth = Dimensions.get('window').width;
-    const [zoomLevel, setZoomLevel] = useState(20)
+      setClusters(clustersData);
+    },
+    200,
+    {maxWait: 200},
+  );
 
-    const handleRegionChange = debounce(async (region: Region) => {
-        if (mapFiler === "Friends") {
-            return
-        }
-        const zoomLevelI = Math.log2(360 * (windowWidth / 256 / region.longitudeDelta)) + 1;
-        setZoomLevel(zoomLevelI);
-        const boundsData = await mapViewRef.current?.getMapBoundaries();
-        if (!boundsData) {
-            return
-        }
-        const clustersData = useSupercluster({
-            points: store.getState().eventsSlice.eventsGeo,
-            bounds: [boundsData?.southWest.longitude - 0.02, boundsData?.southWest.latitude - 0.02, boundsData?.northEast.longitude + 0.02, boundsData?.northEast.latitude + 0.02],
-            zoom: zoomLevel,
-            options: {
-                radius: 75,
-                maxZoom: 40,
-                minZoom: 2,
-                reduce: (accumulated, props) => {
-                    accumulated.ids = [...accumulated.ids, ...props.ids];
-                },
-            },
-        });
+  return (
+    <MapView
+      ref={mapViewRef}
+      rotateEnabled={false}
+      style={{flex: 1}}
+      showsCompass={false}
+      initialRegion={{
+        latitude: userCoordinates?.lat || 0,
+        longitude: userCoordinates?.lng || 0,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      userInterfaceStyle={'light'}
+      showsPointsOfInterest={false}
+      onRegionChange={handleRegionChange}
+      onRegionChangeComplete={handleRegionChangeComplete}>
+      <EventsClusters clusters={clusters} />
+      <FriendsClusters />
+      <UserMarker />
+    </MapView>
+  );
+};
 
-        setClusters(clustersData)
-    }, 200, { maxWait: 200 })
-
-
-
-    return (
-        <MapView
-            ref={mapViewRef}
-            style={{ flex: 1 }}
-            showsCompass={false}
-            initialRegion={{
-                latitude: userCoordinates?.lat || 0,
-                longitude: userCoordinates?.lng || 0,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
-            userInterfaceStyle={"light"}
-            showsPointsOfInterest={false}
-            onRegionChange={handleRegionChange}
-            onRegionChangeComplete={handleRegionChangeComplete}
-        >
-            <EventsClusters clusters={clusters} />
-            <FriendsClusters />
-            <UserMarker />
-        </MapView >
-    )
-
-}
-
-export default MapContent
+export default MapContent;
